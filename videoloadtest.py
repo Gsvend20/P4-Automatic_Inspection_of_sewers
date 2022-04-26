6cdeaeda763c11b10d91a0340998818723dff796
Simon Haaning <73152757+SimonHaaningAAU@users.noreply.github.com>
2022-04-26 14:43:58 +0200

    Example file demonstrating how to use VideoLoader class

---
+++ videoloadtest.py
@@ -0,0 +1,19 @@
+import cv2
+import load_saved_videos
+
+path = 'sewer recordings/2'
+
+loader = load_saved_videos.VideoLoader()
+
+loader.load_videos(path)
+
+while True:
+    bgr_frame, depth_frame, ir_frame, read_success = loader.get_next_frames()
+    if not read_success:
+        exit(1)
+    else:
+        cv2.imshow('color_video', bgr_frame)
+        cv2.imshow('depth_video', depth_frame)
+        cv2.imshow('ir_video', ir_frame)
+
+    cv2.waitKey(10)
