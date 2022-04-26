df442ca04517b19d67dfd1c407e5d2f5e2e03839
Miki Pregaard <48087675+Miki0110@users.noreply.github.com>
2022-04-26 19:23:53 +0200

    Added light_uniformity script

--- tests/histogram.py
+++ tests/histogram.py
@@ -1,28 +1,48 @@
-import cv2 as cv
+import cv2
 from pathlib import Path
 from matplotlib import pyplot as plt
 
 def resize_image(image, image_name, procent):
     [height, width] = [image.shape[0],image.shape[1]]
     [height, width] = [procent*height, procent*width]
-    cv.namedWindow(image_name, cv.WINDOW_NORMAL)
-    cv.resizeWindow(image_name, int(width), int(height))
-    cv.imshow(image_name, image)
+    cv2.namedWindow(image_name, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(image_name, int(width), int(height))
+    cv2.imshow(image_name, image)
 
 
-picPath = Path.cwd().parent.as_posix()+'/materials/GR-type_0_2.jpg'
+for n in range(1,3):
+    v_num = n
+    path_bgr = f'{Path.cwd().parent.as_posix()}/sewer recordings/Training data/Straight pipe/'
 
-img = cv.imread(picPath)
+    vid = cv2.VideoCapture(path_bgr+f'{v_num}_bgr.avi')
+    mean_intensity = 0
+    frame_length = 0
+    max_min_intensity = []
+    while vid.isOpened():
+        # read frame
+        ret, frame = vid.read()
+        if not ret:
+            break
+        resize_image(frame, 'image', 0.4)
 
-color = ('b','g','r')
-b,g,r = cv.split(img)
-for i,col in enumerate(color):
-    histr = cv.calcHist([img],[i],None,[256],[0,256])
-    plt.plot(histr,color = col)
-    plt.xlim([0,256])
-plt.show()
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+        if cv2.waitKey(10) & 0xFF == ord('w'):
 
-resize_image(b, 'blue channel', 0.4)
-resize_image(r, 'red channel', 0.4)
-resize_image(g, 'green channel', 0.4)
-cv.waitKey(0)
+            full_frame = []
+            full_frame.append(frame)
+            full_frame.append(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV))
+            full_frame.append(cv2.cvtColor(frame, cv2.COLOR_BGR2HLS))
+            full_frame.append(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
+
+            color = ('blue', 'green', 'red')
+            fig, axs = plt.subplots(4, figsize=(12, 6))
+            fig.suptitle('bgr, hsv, hsi, grey')
+            for n in range(3):
+                for i, col in enumerate(color):
+                    hist = cv2.calcHist([full_frame[n]], [i], None, [256], [0, 256])
+                    axs[n].plot(hist, color=col)
+                    plt.xlim([0, 256])
+            axs[3].plot(cv2.calcHist([full_frame[3]], [0], None, [256], [0, 256]), color='black')
+            plt.xlim([0, 256])
+            plt.show()
