d9ca3e7a8f49763db7e74e5aa48d5b51f9e018b7
Asbjørn Lauritsen <asbjorn2625@gmail.com>
2022-04-28 14:45:23 +0200

    Update VideotoPng.py

--- Training/VideotoPng.py
+++ Training/VideotoPng.py
@@ -2,12 +2,6 @@ import cv2 as cv
 from os.path import exists
 from os import mkdir, listdir
 
-videos = 5
-types = "down_stream"
-clas = "Branch_training"
-Vision = "bgr"
-files_name = "branch"
-
 def resizenation(image, percentage):
     scale_percent = percentage  # percent of original size #this is just to make the program faster
     width = int(image.shape[1] * scale_percent / 100)
@@ -15,16 +9,31 @@ def resizenation(image, percentage):
     dim = (width, height)
     # resize image
     return cv.resize(image, dim, interpolation=cv.INTER_AREA)
+
+types = "down_stream"
+clas = "Branch_training"
+files_name = "branch"
+trainingname = "branchtrain"
+
 path = "%s/%s/%s" % (clas, clas, types)
 listofvids = listdir(path)
 file_number = 0
 for i in range(int(len(listofvids)/3)):
-    video = "%s/%s/%s/%s_%s.avi" % (clas, clas, types, i, Vision)
-    if exists(video):
-        cap = cv.VideoCapture(video)
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
+    print(f"{clas}/{clas}/{types}/{i+1}_bgr.avi")
+    bgrvideo = f"{clas}/{clas}/{types}/{i+1}_bgr.avi"
+    irvideo = f"{clas}/{clas}/{types}/{i+1}_ir.avi"
+    depthvideo = f"{clas}/{clas}/{types}/{i+1}_depth.avi"
+    if exists(bgrvideo) and exists(irvideo) and exists(depthvideo):
+        if not exists("./%s" % trainingname):
+            mkdir("./%s" % trainingname)
+        bgrcap = cv.VideoCapture(bgrvideo)
+        ircap = cv.VideoCapture(irvideo)
+        depthcap = cv.VideoCapture(depthvideo)
+        while bgrcap.isOpened():
+            ret, frame = bgrcap.read()
+            ret1, irframe = ircap.read()
+            ret2, depthframe = depthcap.read()
+            if not ret and not ret1 and not ret2:
                 print("next video")
                 break
             resized = resizenation(frame, 20)
@@ -32,6 +41,11 @@ for i in range(int(len(listofvids)/3)):
             key = cv.waitKey(10)
             if key == ord("s"):
                 print(file_number)
-                filename = "%s_%s.png" % (files_name, file_number)
-                cv.imwrite(filename, frame)
+                bgrfilename = "%s/%s_%s.png" % (trainingname, "bgr_"+files_name, file_number)
+                irfilename = "%s/%s_%s.png" % (trainingname, "ir_"+files_name, file_number)
+                depthfilename = "%s/%s_%s.png" % (trainingname, "depth_" + files_name, file_number)
+                cv.imwrite(bgrfilename, frame)
+                if irframe == 0:
+                    cv.imwrite(irfilename, irframe)
+                    cv.imwrite(depthfilename, depthframe)
                 file_number = file_number+1
