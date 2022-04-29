c4ab1e64d247ee5df5e45d4224bab9162a9fdfb4
gsvend20 <gsvend20@student.aau.dk>
2022-04-29 13:30:52 +0200

    Inserted vid of Sand with canny+Sobel, and made pause possible

--- tests/Saving func.py
+++ tests/Saving func.py
@@ -1,13 +1,14 @@
 # Import part
 from __future__ import print_function
-from os.path import exists
+# from os.path import exists
 import cv2 as cv
-import pathlib
+# import pathlib
 from Functions import Funcs as func
 import materials.datafile as Vas
 
 
-input_vid = func.GetVid('1_bgr')
+# input_vid = func.GetVid('1_bgr')
+input_vid = func.GetVid(r'C:\Users\Glenn\OneDrive - Aalborg Universitet\P4 - GrisProjekt\Training data\Roots\2nd training set\12_2_1bgr_.avi')
 # Read until video is completed
 while input_vid.isOpened():
     Vas.PicNumb1 = 0
@@ -15,10 +16,20 @@ while input_vid.isOpened():
     # input_vidture frame-by-frame
     ret, frame = input_vid.read()
     if ret:
-        func.PrintPic('vid', frame)
-        Thres_img = func.Threshold(frame)
+        Blur_img = func.Blur(frame, 'Gaussian')
+        Thres_img = func.Threshold(Blur_img)
+        Canny_img = func.Canny(Thres_img)
+        Sobel_img = func.Sobel(Thres_img)
+        # func.PrintPic('vid', frame)
+        func.PrintPic('Blur', Blur_img)
         func.PrintPic('Threshold', Thres_img)
+        func.PrintPic('Egde_img Canny', Canny_img)
+        func.PrintPic('Egde_img Sobel', Sobel_img)
         # cv.imshow('Frame', frame)
+
+        if cv.waitKey(25) & 0xFF == ord('n'):
+            while not cv.waitKey(25) & 0xFF == ord('m'):
+                {}
 
         # Press Q on keyboard to  exit
         if cv.waitKey(25) & 0xFF == ord('q'):
