36923ce8364991113b48f67c81515b133921179c
gsvend20 <gsvend20@student.aau.dk>
2022-04-29 13:58:59 +0200

    Added Datafile

---
+++ materials/datafile.py
@@ -0,0 +1,24 @@
+
+
+# HSV data
+HSVmax_value = 255
+HSVmax_value_H = 360 // 2
+low_H = 60
+low_S = 50
+low_V = 40
+high_H = 150
+high_S = 150
+high_V = 170
+Blur = 7  # for Bluring the image
+ErodeKernel = 0  # Kernel for Erode
+DilatKernel = 0  # Kernel for Dilation
+EgdeKernel = 101  # Kernel for Egdedetection
+EgdeLow = 100
+EgdeHigh = 200
+ResizeSize1 = 450
+ResizeSize2 = 450
+# Saving data variables
+RunID = 0
+PicNumb1 = 0
+PicNumb2 = 0
+
