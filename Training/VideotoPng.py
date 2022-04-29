538e820063cf3b4fbc0fdb88660801ee89881e17
Asbjørn Lauritsen <asbjorn2625@gmail.com>
2022-04-29 15:23:07 +0200

    Update VideotoPng.py

--- Training/VideotoPng.py
+++ Training/VideotoPng.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 from os.path import exists
 from os import mkdir, listdir
+import numpy as np
 
 def resizenation(image, percentage):
     scale_percent = percentage  # percent of original size #this is just to make the program faster
@@ -10,23 +11,29 @@ def resizenation(image, percentage):
     # resize image
     return cv.resize(image, dim, interpolation=cv.INTER_AREA)
 
-types = ["down_stream", "up_stream"]
-clas = "Branch_training"
-files_name = "branch"
-trainingname = "branchtrain"
-mkdir("./%s" % trainingname)
-for j in range(len(types)):
-    path = f"{clas}/{clas}/{types[j]}"
-    listofvids = listdir(path)
-    file_number = 0
-    for i in range(int(len(listofvids)/3)):
-        print(f"{clas}/{clas}/{types[j]}/{i+1}_bgr.avi")
-        bgrvideo = f"{clas}/{clas}/{types[j]}/{i+1}_bgr.avi"
-        irvideo = f"{clas}/{clas}/{types[j]}/{i+1}_ir.avi"
-        depthvideo = f"{clas}/{clas}/{types[j]}/{i+1}_depth.avi"
+folder = '2nd_training_set'
+files_name = "Sand"
+trainingname = "Sandtraindata"
+
+if not exists("./%s" % trainingname):
+    mkdir("./%s" % trainingname)
+path = f"{folder}"
+listofvids = listdir(path)
+path = f"{folder}"
+listofvids = listdir(path)
+
+for j, vid in enumerate(listofvids):
+    spit = vid.split("_")
+    if spit[2] == 'bgr.avi':
+        bgrvideo = f"{folder}/{listofvids[j]}"
+        depthvideo = f"{folder}/{listofvids[j+1]}"
+        irvideo = f"{folder}/{listofvids[j+2]}"
+        types = spit[0]
+        videonum = spit[1]
+        filenum = 0
         if exists(bgrvideo) and exists(irvideo) and exists(depthvideo):
-            if not exists(f"{trainingname}/{types[j]}"):
-                mkdir(f"{trainingname}/{types[j]}")
+            if not exists(f"{trainingname}/{types}"):
+                mkdir(f"{trainingname}/{types}")
             bgrcap = cv.VideoCapture(bgrvideo)
             ircap = cv.VideoCapture(irvideo)
             depthcap = cv.VideoCapture(depthvideo)
@@ -34,19 +41,22 @@ for j in range(len(types)):
                 ret, frame = bgrcap.read()
                 ret1, irframe = ircap.read()
                 ret2, depthframe = depthcap.read()
-                if not ret and not ret1 and not ret2:
+                if not ret:
                     print("next video")
                     break
                 resized = resizenation(frame, 20)
                 cv.imshow("BGR image", resized)
                 key = cv.waitKey(10)
+                if key == ord("q"):
+                    break
                 if key == ord("s"):
-                    print(file_number)
-                    bgrfilename = f"{trainingname}/{types[j]}/bgr_{files_name}_{file_number}.png"
-                    irfilename = f"{trainingname}/{types[j]}/ir_{files_name}_{file_number}.png"
-                    depthfilename = f"{trainingname}/{types[j]}/depth_{files_name}_{file_number}.png"
+                    filenum = str(filenum)
+                    filenum = filenum.zfill(3)
+                    bgrfilename = f"{trainingname}/{types}/{filenum}_bgr_{files_name}_{videonum}.png"
+                    irfilename = f"{trainingname}/{types}/{filenum}_ir_{files_name}_{videonum}.png"
+                    depthfilename = f"{trainingname}/{types}/{filenum}_depth_{files_name}_{videonum}.png"
                     cv.imwrite(bgrfilename, frame)
-                    if irframe != None:
-                        cv.imwrite(irfilename, irframe)
-                        cv.imwrite(depthfilename, depthframe)
-                    file_number = file_number+1
+                    filenum = int(filenum)
+                    filenum = filenum+1
+                    cv.imwrite(irfilename, irframe)
+                    cv.imwrite(depthfilename, depthframe)
